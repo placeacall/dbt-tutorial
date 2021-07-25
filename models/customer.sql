747d2a92e0c82efb3136667d2f59a7d12154ff1a
Emmanuel Obayi <emmanuel.obayi@gmail.com>
2021-07-25 21:17:35 +0000

    changes to customer.sql

--- models/customer.sql
+++ models/customer.sql
@@ -1,18 +1,12 @@
-
 with customers as (
 
-    select *
-    from {{ ref('stg_customers') }}
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from {{ ref('stg_orders') }}
+
+    select * from {{ ref('stg_orders') }}
 
 ),
 
